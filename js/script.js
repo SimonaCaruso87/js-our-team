8b7dd3df739348544a40f74dfe0ad1a85c672477
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-06-26 17:54:25 +0200

    Add ciclo array object

---
+++ js/script.js
@@ -0,0 +1,76 @@
+/* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 0:
+Creare l'array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede
+Consigli del giorno:
+Ragioniamo come sempre a step.
+Prima la logica in italiano e poi traduciamo in codice.
+E ricordiamoci che console.log() è nostro amico!
+Buon lavoro! */
+
+/*
+NOME            RUOLO                   IMMAGINE
+Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
+Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
+Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
+Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+Scott Estrada	Developer	            scott-estrada-developer.jpg
+Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
+*/
+
+//MILESTONE 0:
+//Creare l'array di oggetti con le informazioni fornite.
+
+const arrayObject = [
+  {
+    nome : 'Wayne Barnett',
+    ruolo : 'Founder & CEO',
+    immagine : 'wayne-barnett-founder-ceo.jpg'
+  }
+  {
+    nome : 'Angela Caroll',
+    ruolo : 'Chief Editor',
+    immagine : 'angela-caroll-chief-editor.jpg'
+  }
+  {
+    nome : 'Walter Gordon',
+    ruolo : 'Office Manager',
+    immagine : 'walter-gordon-office-manager.jpg'
+  }
+  {
+    nome : 'Angela Lopez',
+    ruolo : 'Social Media Manager',
+    immagine : 'angela-lopez-social-media-manager.jpg'
+  }
+  {
+    nome : 'Scott Estrada',
+    ruolo : 'Developer',
+    immagine : 'scott-estrada-developer.jpg'
+  }
+  {
+    nome : 'Barbara Ramos',
+    ruolo : 'Graphic designer',
+    immagine : 'barbara-ramos-graphic-designer.jpg'
+  }
+]
+   
+console.log(arrayObject ,"Oggetti dell'array" , typeof arrayObject );
+
+//MILESTONE 1:
+//Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+
+
+//MILESTONE 2:
+//Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+
